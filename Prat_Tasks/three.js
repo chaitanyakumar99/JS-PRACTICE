c5f00ec24346e664aefbfba84451264c72943590
USER 1 <gchkumar999@gmail.com>
2024-04-30 16:12:53 +0530

    txt

--- Prat_Tasks/three.js
+++ Prat_Tasks/three.js
@@ -1,30 +1,11 @@
-// function findLargeWord(str){
-// let newstrarr=str.split(" ")
-// //console.log(newstrarr);
+function findlargeword(str){
+      let newstrr=str.split(" ")
+     // console.log(largewordarr);
 
-//  let largeword =  newstrarr.reduce((curr,acc)=>{
-   
-//     return acc.length>curr.length ? acc :curr
-// })
-//     return largeword
-// }
-// largeword=findLargeWord("chai is  very good developer")
-// console.log(largeword);
-
-
-function findLargeword(str){
-let newstrarr=str.split(" ")
-//console.log(newstrarr);
-
-
-let Largeword =newstrarr.reduce((current,accumulator)=>{
-
-    return accumulator.length>current.length ? accumulator:current
-})
-
-return Largeword 
-
+    let newarr=newstrr.reduce((accumulatevalue,currentvalue)=>{
+        return accumulatevalue.length>currentvalue.length ? accumulatevalue:currentvalue
+    })
+    return newarr
 }
-
-result=findLargeword("Rahul is very good developer")
+result=findlargeword("Rahul is a good developer")
 console.log(result);
