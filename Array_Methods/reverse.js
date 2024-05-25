baa247936e15994945abede445a4df11d9998229
USER 1 <gchkumar999@gmail.com>
2024-05-25 21:55:17 +0530

    prat

---
+++ Array_Methods/reverse.js
@@ -0,0 +1,41 @@
+// let num=[100,200,300,400]
+// let names=["Rahul","Varma","kumar"]
+// let myname=["chaitanya"]
+// let year=[2024]
+
+
+// num.reverse()
+// names.reverse()
+// myname.split
+
+
+// console.log(num);
+// console.log(names);
+// console.log(myname);
+// console.log(year);
+
+//-----------------------------------------------------------------------//
+
+//find the highest num
+
+//find the long word
+
+//find the difference between highest and lowest num
+
+
+let str ="chaitanya is a good boy"
+function chai(){
+    let d = str.split(" ")
+    let h = d.sort((a,b)=>b.length-a.length)
+    return  h.at()
+}
+let  g = chai(str)
+console.log(g)
+
+let a =[23,0,4,5,7,89,54]
+function sorting(){
+    let c = a.sort((a,b)=>b-a)
+    return c.at()
+}
+let j=sorting(a)
+console.log(j)
