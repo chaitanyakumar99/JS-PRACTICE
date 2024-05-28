b43da892d0c1d4d02e377e69af772ca9746af168
USER 1 <gchkumar999@gmail.com>
2024-05-28 18:36:43 +0530

    prat

--- ascending_order/two.js
+++ ascending_order/two.js
@@ -1,6 +1,22 @@
 // ascending order :-
 
+// const nbr=[50,20,70,30,15,80,2]
+
+// const result=nbr.sort((a,b)=>a-b);
+// console.log(result);
+
+
+// ------------------------------------------------------------------------------
+
 const nbr=[50,20,70,30,15,80,2]
 
-const result=nbr.sort((a,b)=>a-b);
+const result=nbr.sort((a,b)=>{
+    if (a>=b){
+        return 1;
+    }
+    else {
+        return -1;
+    }
+
+});
 console.log(result);
