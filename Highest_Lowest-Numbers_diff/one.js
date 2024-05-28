8fa49162d7c4bdacc4e2dad50d3eb6fbd16420af
USER 1 <gchkumar999@gmail.com>
2024-05-28 22:02:38 +0530

    prat

--- Highest_Lowest-Numbers_diff/one.js
+++ Highest_Lowest-Numbers_diff/one.js
@@ -1,6 +1,17 @@
+
+//Highest and  Lowest Numbers differences
+
+// const nbr=[1,55,90,20,40,80,60]
+
+// const result=Math.max(...nbr);
+// const result1=Math.min(...nbr);
+
+// console.log(result - result1);
+
 const nbr=[1,55,90,20,40,80,60]
 
-const result=Math.max(...nbr);
-const result1=Math.min(...nbr);
+const sortarr=nbr.sort((a,b)=>a-b)
+const Maxnbr=sortarr[sortarr.length-1]
+const Minnbr=sortarr[0]
 
-console.log(result - result1);
+console.log(Maxnbr - Minnbr);
