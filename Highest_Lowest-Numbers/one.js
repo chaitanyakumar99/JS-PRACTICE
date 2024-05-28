22dc37f292a14e88abc32fb05621a817782bbd60
USER 1 <gchkumar999@gmail.com>
2024-05-28 19:45:16 +0530

    prat

--- Highest_Lowest-Numbers/one.js
+++ Highest_Lowest-Numbers/one.js
@@ -12,3 +12,12 @@ const nbr1=[1,55,90,20,40,80,60]
 const sortarr=nbr1.sort((a,b)=>a-b)
 const maxnbr=sortarr[sortarr.length-1]
 console.log(maxnbr);
+
+
+// Lowest Number :-
+
+const nbr=[1,55,90,20,40,80,60]
+
+const result=Math.min(...nbr);
+console.log(result);
+
