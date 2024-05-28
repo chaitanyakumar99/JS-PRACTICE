6f6a1deacd5980a047b6dedfd120bd8088b2b4a1
USER 1 <gchkumar999@gmail.com>
2024-05-28 18:44:41 +0530

    prat

--- descending_order/one.js
+++ descending_order/one.js
@@ -1,13 +1,20 @@
 // descding order :-
 
+// const nbr=[10,55,90,20,40,80,60]
+
+// const result =nbr.sort((a,b)=>{
+//     if (b>=a){
+//         return 1;
+//     }
+//     else{
+//         return -1;
+//     }
+// });
+// console.log(result);
+
+// -----------------------------------------------------------------
+
 const nbr=[10,55,90,20,40,80,60]
 
-const result =nbr.sort((a,b)=>{
-    if (b>=a){
-        return 1;
-    }
-    else{
-        return -1;
-    }
-});
+const result=nbr.sort((a,b)=>b-a)
 console.log(result);
