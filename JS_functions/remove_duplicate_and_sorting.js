0e265bd6de85a982b980df6a246fe6c6eb4526ad
USER 1 <gchkumar999@gmail.com>
2024-06-05 20:07:21 +0530

    remove duplicates and sorting

--- JS_functions/remove_duplicate_and_sorting.js
+++ JS_functions/remove_duplicate_and_sorting.js
@@ -3,7 +3,7 @@ const duplicates=[9,4,9,4,6,7,2,1,6]
 function removeduplicatesorting(arr){
     const new1 =Array.from(new Set(arr));
     
-    // sorting
+    // sorting the duplicates
 
     new1.sort((a,b)=>a-b);
   
