3f1f2e9d1e67f5fe0918501c83b0f5ec497bb0f4
USER 1 <gchkumar999@gmail.com>
2024-06-05 19:32:08 +0530

    remove duplicates

--- JS_functions/remove_duplicates.js
+++ JS_functions/remove_duplicates.js
@@ -2,7 +2,8 @@ const duplicates=[9,4,9,4,6,7,2,1,6]
 
 function removedupl(arr){
 
-    return Array.from(new Set(arr));  
+    return Array.from(new Set(arr));  // 'Set' objecct is used to store unique values
+                                      // the 'Array.from' method is used to convert the 'set' back into an array
 }
 
 console.log(removedupl(duplicates));
