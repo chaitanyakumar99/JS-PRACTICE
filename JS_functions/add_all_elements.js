6f2d65193d5df564f07b1c7a53d0c67a2da3c23b
USER 1 <gchkumar999@gmail.com>
2024-06-06 18:25:47 +0530

    add all elements of the array

--- JS_functions/add_all_elements.js
+++ JS_functions/add_all_elements.js
@@ -1,16 +1,20 @@
+
+// add all elements of the array
+
 const array1=[1,2,,3,4,5,6,7,8]
 
 
 function add(array){
-let sum = 0;
+// let sum = 0;
 
-for(let i=0 ; i < array1.length; i++){
-    if (array[i]){
-        sum += array[i] // is used to accumulate the sum of the array elements . 
-                        //'+=' is a operator in javascript that combines additiion and assignment in a single step. 
-    }
-}
-  return sum;
+// for(let i=0 ; i < array1.length; i++){
+//     if (array[i]){
+//         sum += array[i] // is used to accumulate the sum of the array elements . 
+//                         //'+=' is a operator in javascript that combines additiion and assignment in a single step. 
+//     }
+// }
+//   return sum;
+   return array1.reduce((a,b)=>a+b)  // reduce method will return one single output from the single array
 }
 
 console.log(add(array1));
