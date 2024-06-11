570566e6acb94ff33cfc5c81fa51440d70894dbb
USER 1 <gchkumar999@gmail.com>
2024-06-11 09:37:38 +0530

    length of the string

---
+++ JS_functions/find_length_of_string.js
@@ -0,0 +1,35 @@
+
+
+
+
+// let name1="developer";
+
+
+// let lengthOfString = name1.length;
+
+ 
+
+
+
+let name1="developer";
+
+function getStringLength(str){
+    return str.length
+}
+
+let lengthOfName1 = getStringLength(name1);
+
+console.log(lengthOfName1);
+
+
+// let language = "JavaScript";
+
+// let count = 0;
+// for (let x of language) {
+//     console.log(x)
+// count = count + 1;
+
+// }
+
+// console.log(count);
+
