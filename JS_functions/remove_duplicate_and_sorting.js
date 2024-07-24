a98c0dd338c1e7d8ee1b3e9724d9627a37de7182
USER 1 <gchkumar999@gmail.com>
2024-07-24 19:32:40 +0530

    txt

--- JS_functions/remove_duplicate_and_sorting.js
+++ JS_functions/remove_duplicate_and_sorting.js
@@ -4,6 +4,7 @@
 const duplicates=[9,4,9,4,6,7,2,1,6]
 
 function removeduplicatesorting(arr){
+    
     const new1 =Array.from(new Set(arr));    // 'Set' object is used to store unique values . By passing the 'arr' to the 'Set' constructor, duplicates are automatically removed because 'Set' allows only unique values
                                              // the 'Array.from' method is used to convert the 'Set' back into an array
 
