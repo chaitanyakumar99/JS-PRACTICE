f4436075ac8d05c3e768f5f73f2fc3fe92af6e4f
chaitanyakumar99 <gchkumar999@gmail.com>
2025-01-02 17:58:48 +0530

    txt

---
+++ Destructuring/function_array1.js
@@ -0,0 +1,9 @@
+function getuserData() {
+    return ["pavan", "Hyderabad", 4500]
+}
+
+const[name,city,sal]=getuserData()
+
+console.log(`Name:${name}`)
+console.log(`City:${city}`)
+console.log(`City:${sal}`)
