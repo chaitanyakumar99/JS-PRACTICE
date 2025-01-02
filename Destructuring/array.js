bffb2f7e95e4e92ed094c40ddc7150cdf017efc9
chaitanyakumar99 <gchkumar999@gmail.com>
2025-01-02 10:48:19 +0530

    txt

---
+++ Destructuring/array.js
@@ -0,0 +1,5 @@
+const arr=["rahul","hyderabad",45000]
+
+const [name,loc,sal]=arr
+
+console.log(name,loc,sal)
---
+++ Destructuring/obj.js
@@ -0,0 +1,11 @@
+const obj={
+    name:"varma",
+    loc:"Bangalore",
+    sal:75000
+}
+
+const {name,loc,sal}=obj
+console.log(name,loc,sal)
+
+
+console.log(obj.name)
